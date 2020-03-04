bd5a420d41a9064e6e69882eae103b7f08747b28
Ghanshyam0110 <ghanshyam0110@gmail.com>
2020-03-04 10:50:50 +0100

    update gitignore file

--- .sfdx/typings/lwc/staticresources.d.ts
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -1,4 +1,56 @@
+declare module "@salesforce/resourceUrl/AngularJS" {
+    var AngularJS: string;
+    export default AngularJS;
+}
+declare module "@salesforce/resourceUrl/Houseboats" {
+    var Houseboats: string;
+    export default Houseboats;
+}
+declare module "@salesforce/resourceUrl/Leaflet" {
+    var Leaflet: string;
+    export default Leaflet;
+}
+declare module "@salesforce/resourceUrl/PleasureBoats" {
+    var PleasureBoats: string;
+    export default PleasureBoats;
+}
+declare module "@salesforce/resourceUrl/ReadMe" {
+    var ReadMe: string;
+    export default ReadMe;
+}
+declare module "@salesforce/resourceUrl/Sailboats" {
+    var Sailboats: string;
+    export default Sailboats;
+}
 declare module "@salesforce/resourceUrl/YeurDreaminLogo" {
     var YeurDreaminLogo: string;
     export default YeurDreaminLogo;
+}
+declare module "@salesforce/resourceUrl/boats" {
+    var boats: string;
+    export default boats;
+}
+declare module "@salesforce/resourceUrl/cti_sprite" {
+    var cti_sprite: string;
+    export default cti_sprite;
+}
+declare module "@salesforce/resourceUrl/fivestar" {
+    var fivestar: string;
+    export default fivestar;
+}
+declare module "@salesforce/resourceUrl/phone_dialing_mp3" {
+    var phone_dialing_mp3: string;
+    export default phone_dialing_mp3;
+}
+declare module "@salesforce/resourceUrl/phone_ring_mp3" {
+    var phone_ring_mp3: string;
+    export default phone_ring_mp3;
+}
+declare module "@salesforce/resourceUrl/realtyapp" {
+    var realtyapp: string;
+    export default realtyapp;
+}
+declare module "@salesforce/resourceUrl/yuiBuildMin" {
+    var yuiBuildMin: string;
+    export default yuiBuildMin;
 }
